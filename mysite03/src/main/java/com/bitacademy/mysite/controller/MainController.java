88fc1358aa77856c2fcfa637ae91ae6159fbe895
junnyda <junnyda1004@gmail.com>
2022-11-16 15:44:09 +0900

    guestbook#1 & board#1

--- mysite03/src/main/java/com/bitacademy/mysite/controller/MainController.java
+++ mysite03/src/main/java/com/bitacademy/mysite/controller/MainController.java
@@ -5,10 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-	
-	@RequestMapping({"","/main"})
+	@RequestMapping({"", "/main"})
 	public String index() {
 		return "main/index";
 	}
-	
 }
