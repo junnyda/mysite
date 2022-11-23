b99cca46f549e7af0f31af4ba53afbb3f45b8fba
junnyda <junnyda1004@gmail.com>
2022-11-23 10:13:20 +0900

    gallery #finish

--- mysite03/src/main/java/com/bitacademy/mysite/controller/GalleryController.java
+++ mysite03/src/main/java/com/bitacademy/mysite/controller/GalleryController.java
@@ -1,31 +1,40 @@
 package com.bitacademy.mysite.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.bitacademy.mysite.service.FileUploadService;
-import com.bitacademy.mysite.service.GalleryService;
+import com.bitacademy.mysite.service.GalleyService;
 import com.bitacademy.mysite.vo.GalleryVo;
 
 @Controller
 @RequestMapping("/gallery")
 public class GalleryController {
 	@Autowired
-	private GalleryService galleryService;
+	private GalleyService galleryService;
 	
 	@Autowired
 	private FileUploadService fileuploadService;
 	
 	@RequestMapping("")
-	public String index() {
+	public String index(Model model) {
+		List<GalleryVo> list = galleryService.getImageList();
+		model.addAttribute("list", list);
+
 		return "gallery/index";
 	}
 	
 	@RequestMapping("/upload")
-	public String upload(GalleryVo galleryVo, MultipartFile multipartFile) {
+	public String upload(
+		GalleryVo galleryVo,
+		@RequestParam("file") MultipartFile multipartFile) {
 		String url = fileuploadService.restore(multipartFile);
 		galleryVo.setUrl(url);
 		
