c3189e9a6045a08a521461a3e269b1fb7512593b
junnyda <junnyda1004@gmail.com>
2022-11-23 14:11:45 +0900

    stop watch

--- mysite03/src/main/java/com/bitacademy/mysite/repository/UserRepository.java
+++ mysite03/src/main/java/com/bitacademy/mysite/repository/UserRepository.java
@@ -2,9 +2,12 @@ package com.bitacademy.mysite.repository;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StopWatch;
+
 import com.bitacademy.mysite.vo.UserVo;
 
 @Repository
@@ -22,10 +25,19 @@ public class UserRepository {
 	}
 	
 	public UserVo findByEmailAndPassword(String email, String password) {
+		StopWatch sw = new StopWatch();
+		sw.start();
+		
 		Map<String, Object> map = new HashMap<>();
 		map.put("email", email);
 		map.put("password", password);
-		return sqlSession.selectOne("user.findByEmailAndPassword", map);
+		UserVo result = sqlSession.selectOne("user.findByEmailAndPassword", map);
+
+		sw.stop();
+		Long totalTime = sw.getTotalTimeMillis();
+		System.out.println("----->" + totalTime);
+		
+		return result;
 	}
 	
 	public Boolean insert(UserVo vo) {
